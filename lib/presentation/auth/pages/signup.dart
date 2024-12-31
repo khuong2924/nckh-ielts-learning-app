a02cbf58b710f11a1332a25a4c369e0cae1be3f8
azur-14 <hieungan0906@gmail.com>
2024-12-31 19:30:12 +0700

    nhung lai firebase

--- lib/presentation/auth/pages/signup.dart
+++ lib/presentation/auth/pages/signup.dart
@@ -8,33 +8,58 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SignupPage extends StatelessWidget {
+import 'package:auth/common/bloc/button/button_state.dart';
+import 'package:auth/common/bloc/button/button_state_cubit.dart';
+import 'package:auth/common/widgets/button/basic_app_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class SignupPage extends StatefulWidget {
   SignupPage({super.key});
 
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
   final TextEditingController _usernameCon = TextEditingController();
   final TextEditingController _emailCon = TextEditingController();
   final TextEditingController _passwordCon = TextEditingController();
+
+  bool _obscurePassword = true; // Biến để kiểm soát hiển thị mật khẩu
+
+  @override
+  void dispose() {
+    _usernameCon.dispose();
+    _emailCon.dispose();
+    _passwordCon.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: BlocProvider(
         create: (context) => ButtonStateCubit(),
-        child: BlocListener<ButtonStateCubit,ButtonState>(
+        child: BlocListener<ButtonStateCubit, ButtonState>(
           listener: (context, state) {
             if (state is ButtonSuccessState) {
               Navigator.pushReplacement(
                 context,
-                MaterialPageRoute(builder: (context) => const HomePage(),)
+                MaterialPageRoute(builder: (context) => const HomePage()),
               );
             }
-            if (state is ButtonFailureState){
+            if (state is ButtonFailureState) {
               var snackBar = SnackBar(content: Text(state.errorMessage));
               ScaffoldMessenger.of(context).showSnackBar(snackBar);
             }
           },
           child: SafeArea(
-            minimum: const EdgeInsets.only(top: 100,right: 16,left: 16),
+            minimum: const EdgeInsets.only(top: 100, right: 16, left: 16),
             child: SingleChildScrollView(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -51,22 +76,23 @@ class SignupPage extends StatelessWidget {
                   const SizedBox(height: 60,),
                   _createAccountButton(context),
                   const SizedBox(height: 20,),
-                   _signinText(context)
+                  _signinText(context)
                 ],
               ),
             ),
           ),
         ),
-      ) ,
+      ),
     );
   }
-  Widget _signup(){
+
+  Widget _signup() {
     return const Text(
       'Sign Up',
       style: TextStyle(
-        color: Color(0xff2A4ECA),
-        fontWeight: FontWeight.bold,
-        fontSize: 32
+          color: Color(0xff2A4ECA),
+          fontWeight: FontWeight.bold,
+          fontSize: 32
       ),
     );
   }
@@ -75,7 +101,7 @@ class SignupPage extends StatelessWidget {
     return TextField(
       controller: _usernameCon,
       decoration: const InputDecoration(
-        hintText: 'Username'
+          hintText: 'Name'
       ),
     );
   }
@@ -84,7 +110,7 @@ class SignupPage extends StatelessWidget {
     return TextField(
       controller: _emailCon,
       decoration: const InputDecoration(
-        hintText: 'Email'
+          hintText: 'Email'
       ),
     );
   }
@@ -92,8 +118,19 @@ class SignupPage extends StatelessWidget {
   Widget _password() {
     return TextField(
       controller: _passwordCon,
-      decoration: const InputDecoration(
-        hintText: 'Password'
+      obscureText: _obscurePassword, // Sử dụng biến để điều khiển hiển thị mật khẩu
+      decoration: InputDecoration(
+        hintText: 'Password',
+        suffixIcon: IconButton(
+          icon: Icon(
+            _obscurePassword ? Icons.visibility : Icons.visibility_off,
+          ),
+          onPressed: () {
+            setState(() {
+              _obscurePassword = !_obscurePassword; // Đảo ngược trạng thái khi nhấn nút
+            });
+          },
+        ),
       ),
     );
   }
@@ -103,44 +140,81 @@ class SignupPage extends StatelessWidget {
       builder: (context) {
         return BasicAppButton(
           title: 'Create Account',
-          onPressed: (){
-          }
+          onPressed: () async {
+            String username = _usernameCon.text.trim();
+            String email = _emailCon.text.trim();
+            String password = _passwordCon.text.trim();
+
+            if (email.isEmpty || password.isEmpty || username.isEmpty) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Please fill all fields!')),
+              );
+              return;
+            }
+
+            try {
+              UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                email: email,
+                password: password,
+              );
+
+              // Lưu thông tin người dùng vào Firestore
+              await FirebaseFirestore.instance.collection('users').doc(userCredential.user?.uid).set({
+                'username': username,
+                'email': email,
+              });
+
+              // Phát ra trạng thái thành công
+              context.read<ButtonStateCubit>().emit(ButtonSuccessState());
+
+            } on FirebaseAuthException catch (e) {
+              String errorMessage = 'An error occurred';
+              if (e.code == 'weak-password') {
+                errorMessage = 'The password provided is too weak.';
+              } else if (e.code == 'email-already-in-use') {
+                errorMessage = 'The account already exists for that email.';
+              }
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(errorMessage)),
+              );
+            } catch (e) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("An unexpected error occurred.")),
+              );
+            }
+          },
         );
-      }
+      },
     );
   }
 
-  Widget _signinText(BuildContext context){
+  Widget _signinText(BuildContext context) {
     return Text.rich(
       TextSpan(
         children: [
           const TextSpan(
-            text: 'Do you have account?',
+            text: 'Do you have an account?',
             style: TextStyle(
               color: Color(0xff3B4054),
-              fontWeight: FontWeight.w500
-            )
+              fontWeight: FontWeight.w500,
+            ),
           ),
-           TextSpan(
+          TextSpan(
             text: ' Sign In',
             style: const TextStyle(
               color: Color(0xff3461FD),
-              fontWeight: FontWeight.w500
+              fontWeight: FontWeight.w500,
             ),
             recognizer: TapGestureRecognizer()
-                ..onTap = () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SigninPage(),
-                  )
+              ..onTap = () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SigninPage()),
                 );
-              }
-          )
-        ]
+              },
+          ),
+        ],
       ),
     );
   }
-
-
-
 }
