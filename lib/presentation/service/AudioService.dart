04fce8c660f7e27f9baf6c836b4baf7acc674e5e
khuong2924 <truongkhuong666@gmail.com>
2025-01-13 20:45:53 +0700

    add audio service

---
+++ lib/presentation/service/AudioService.dart
@@ -0,0 +1,13 @@
+class AudioService {
+  static Future<void> loadAudio(String url) async {
+
+  }
+
+  static Future<void> playAudio() async {
+
+  }
+
+  static Future<void> pauseAudio() async {
+
+  }
+}
