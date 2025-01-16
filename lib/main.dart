e1bd0cd62116a729e850ffa243876c298d81678f
khuong2924 <truongkhuong666@gmail.com>
2025-01-17 00:14:11 +0700

    Save local changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auth/presentation/pages/test-page/listening-page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart'; // File được tạo bởi Firebase CLI
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const ReadingHome(), // Để lại SigninPage như cũ
+      home: const ListeningTestPage() // Để lại SigninPage như cũ
     );
   }
 }
